aa23afa0cdbf43ace68d78773e1c71715a5985c4
Emisael Carrera <emisaelcarrera@MacBook-Pro-de-Modyo.local>
2020-03-24 16:27:49 -0300

    ci[plain-refactor]: movements-panel refactor

--- src/components/MovementsPanel.vue
+++ src/components/MovementsPanel.vue
@@ -14,7 +14,7 @@
       </h4>
     </div>
     <div class="row no-gutters">
-      <div class="col-lg-4">
+      <div class="col-lg-4 border-right">
         <div class="movements-panel__filters p-4">
           <div class="movements-panel__filters-dates">
             <h4
@@ -42,160 +42,197 @@
                 :format-header="monthFormat"
                 range-header-text="De %d a %d"
                 range-input-text="De %d a %d"
-                @input="applyDates" />
+                @input="applyDates">
+                <template #activator="{ date }">
+                  <button
+                    ref="activator"
+                    class="form-control btn btn-white border d-flex justify-content-between
+                    align-items-center"
+                    type="button">
+                    <span class="text-capitalize">{{ date }}</span>
+                    <font-awesome-icon
+                      icon="calendar"
+                      class="ml-2" />
+                  </button>
+                </template>
+              </vue-date-picker>
             </div>
           </div>
           <div class="d-none d-lg-block movements-panel__filters-movement-type">
-            <h4 class="mb-4">
+            <h4 class="text-primary">
               <strong>{{ $t('movements.movement-types') }}</strong>
             </h4>
-            <div class="form-check mb-1">
+            <div class="custom-control custom-checkbox">
               <input
                 id="filterByAll"
                 v-model="filterByAll"
-                class="form-check-input"
+                value="filterByAll"
                 type="checkbox"
-                value="all"
+                class="custom-control-input rounded-0"
                 @change="checkAll">
               <label
-                for="filterByAll"
-                class="form-check-label">{{ $t('movements.types.all') }}</label>
+                class="custom-control-label"
+                for="filterByAll">
+                {{ $t('movements.types.all') }}
+              </label>
             </div>
-            <div class="form-check mb-1">
+            <div class="custom-control custom-checkbox">
               <input
                 id="buy-stock"
                 v-model="filterBy"
-                class="form-check-input"
-                type="checkbox"
                 value="buy-stock"
+                type="checkbox"
+                class="custom-control-input rounded-0 filter-by"
                 @change="checkFilters">
               <label
-                for="buy-stock"
-                class="form-check-label">{{ $t('movements.types.stock-purchase') }}</label>
+                class="custom-control-label"
+                for="buy-stock">
+                {{ $t('movements.types.stock-purchase') }}
+              </label>
             </div>
-            <div class="form-check mb-1">
+            <div class="custom-control custom-checkbox">
               <input
                 id="sell-stock"
                 v-model="filterBy"
-                class="form-check-input"
-                type="checkbox"
                 value="sell-stock"
+                type="checkbox"
+                class="custom-control-input rounded-0 filter-by"
                 @change="checkFilters">
               <label
-                for="sell-stock"
-                class="form-check-label">{{ $t('movements.types.stock-sell') }}</label>
+                class="custom-control-label"
+                for="sell-stock">
+                {{ $t('movements.types.stock-sell') }}
+              </label>
             </div>
-            <div class="form-check mb-1">
+            <div class="custom-control custom-checkbox">
               <input
                 id="purchase-mutual-fund"
                 v-model="filterBy"
-                class="form-check-input"
-                type="checkbox"
                 value="purchase-mutual-fund"
+                type="checkbox"
+                class="custom-control-input rounded-0 filter-by"
                 @change="checkFilters">
               <label
-                for="purchase-mutual-fund"
-                class="form-check-label">{{ $t('movements.types.mutual-funds-contribution') }}</label>
+                class="custom-control-label"
+                for="purchase-mutual-fund">
+                {{ $t('movements.types.mutual-funds-contribution') }}
+              </label>
             </div>
-            <div class="form-check mb-1">
+            <div class="custom-control custom-checkbox">
               <input
                 id="redeem-mutual-fund"
                 v-model="filterBy"
-                class="form-check-input"
-                type="checkbox"
                 value="redeem-mutual-fund"
+                type="checkbox"
+                class="custom-control-input rounded-0 filter-by"
                 @change="checkFilters">
               <label
-                for="redeem-mutual-fund"
-                class="form-check-label">{{ $t('movements.types.mutual-funds-rescue') }}</label>
+                class="custom-control-label"
+                for="redeem-mutual-fund">
+                {{ $t('movements.types.mutual-funds-rescue') }}
+              </label>
             </div>
-            <div class="form-check mb-1">
+            <div class="custom-control custom-checkbox">
               <input
                 id="deposit-cash"
                 v-model="filterBy"
-                class="form-check-input"
-                type="checkbox"
                 value="deposit-cash"
+                type="checkbox"
+                class="custom-control-input rounded-0 filter-by"
                 @change="checkFilters">
               <label
-                for="deposit-cash"
-                class="form-check-label">{{ $t('movements.types.cash-payment') }}</label>
+                class="custom-control-label"
+                for="deposit-cash">
+                {{ $t('movements.types.cash-payment') }}
+              </label>
             </div>
-            <div class="form-check mb-1">
+            <div class="custom-control custom-checkbox">
               <input
                 id="withdraw-cash"
                 v-model="filterBy"
-                class="form-check-input"
-                type="checkbox"
                 value="withdraw-cash"
+                type="checkbox"
+                class="custom-control-input rounded-0 filter-by"
                 @change="checkFilters">
               <label
-                for="withdraw-cash"
-                class="form-check-label">{{ $t('movements.types.dividend') }}</label>
+                class="custom-control-label"
+                for="withdraw-cash">
+                {{ $t('movements.types.dividend') }}
+              </label>
             </div>
-            <div class="form-check">
+            <div class="custom-control custom-checkbox">
               <input
                 id="check-others"
                 v-model="filterBy"
-                class="form-check-input"
                 type="checkbox"
-                value="others"
+                class="custom-control-input rounded-0 filter-by"
                 @change="checkFilters">
               <label
-                for="check-others"
-                class="form-check-label">{{ $t('movements.types.others') }}</label>
+                class="custom-control-label"
+                for="check-others">
+                {{ $t('movements.types.others') }}
+              </label>
             </div>
           </div>
           <a
             href="#"
-            class="btn btn-secondary d-none d-lg-block mt-4">{{ $t('movements.export-excel') }}</a>
+            class="btn btn-secondary d-none d-lg-block mt-4">
+            {{ $t('movements.export-excel') }}
+            <font-awesome-icon
+              icon="file-excel"
+              size="lg"
+              class="ml-3" />
+          </a>
         </div>
       </div>
-      <div class="col-lg-8 movements-panel__content">
+      <div class="col-lg-8 movements-panel__content p-1">
         <div
           v-if="isLoading"
           class="spinner-border"
           role="status">
           <span class="sr-only">Loading...</span>
         </div>
-        <div v-else>
-          <div v-if="!movements.length">
-            <div class="p-3 p-lg-5">
-              <h5 class="mb-0">
-                {{ $t('movements.no-movements') }}
-              </h5>
-              <p class="text-muted">
-                {{ $t('movements.select-other-period') }}
-              </p>
-            </div>
+        <div
+          v-else
+          class="h-100 d-flex flex-column justify-content-between border">
+          <div
+            v-if="!movements.length"
+            class="d-flex flex-column justify-content-center p-4 h-100">
+            <h5 class="mb-0 text-center">
+              {{ $t('movements.no-movements') }}
+            </h5>
+            <p class="text-muted text-center">
+              {{ $t('movements.select-other-period') }}
+            </p>
           </div>
           <div
             v-else
-            id="movements-accordion"
-            class="p-3">
+            id="movements-accordion">
             <div
               v-for="movement in movements"
               :key="movement.id"
-              class="movements-panel__row">
+              class="movements-panel__row bg-white border-bottom">
               <a
                 :href="'#movements-' + movement.id"
                 :aria-controls="'#movements-' + movement.id"
                 data-toggle="collapse"
                 aria-expanded="false"
-                class="row no-gutters collapsed"
+                class="row no-gutters collapsed py-2 px-4 btn d-flex rounded-0 position-relative"
                 tabindex="0">
-                <div class="col-lg-2"><span class="pl-lg-3">{{ movement.date | date }}</span></div>
-                <div class="col-lg-7 "><strong>{{ movement.description }}</strong></div>
+                <div class="col-lg-3 text-left"><span>{{ movement.date | date }}</span></div>
+                <div class="col-lg-6 text-left"><strong>{{ movement.description }}</strong></div>
                 <div class="col-lg-3 text-right">
                   <span v-if="movement.type === 'CARGO'">-</span>
                   {{ movement.amount.valueString }}
+                  <font-awesome-icon
+                    icon="chevron-up"
+                    class="ml-2" />
                 </div>
               </a>
               <div
                 :id="'movements-' + movement.id"
                 role="tabpanel"
-                class="collapse"
-                style=""
+                class="collapse py-2 px-4 border-top movements-panel__row-details"
                 data-parent="#movements-accordion">
                 <div
                   v-if="movement.subject == 'cash'"
@@ -247,7 +284,7 @@
                       <strong>{{ movement.priceString }}</strong>
                     </p>
                   </div>
-                  <div class="col">
+                  <div class="col text-lg-right">
                     <small class="d-block">{{ $t('movements.total-amount') }}</small>
                     <p class="mb-0">
                       <strong>{{ movement.amount.valueString }}</strong>
@@ -276,7 +313,7 @@
                       <strong>{{ movement.priceString }}</strong>
                     </p>
                   </div>
-                  <div class="col">
+                  <div class="col text-lg-right">
                     <small class="d-block">{{ $t('movements.total-amount') }}</small>
                     <p class="mb-0">
                       <strong>{{ movement.amount.valueString }}</strong>
@@ -289,70 +326,71 @@
 
           <div
             v-if="filterByAll && movements.length"
-            :class="activeSummary ? 'active' : ''">
-            <div>
-              <a
-                href="#"
-                @click.prevent="openSummary()">
-                <h4 class="mb-0"><strong>{{ $t('movements.period-resume') }}</strong></h4>
-                <font-awesome-icon
-                  :icon="['fas', activeSummary ? 'times' : 'chevron-up']" />
-              </a>
-              <div
-                v-if="activeSummary === true"
-                class="p-2 p-lg-4">
+            class="movements-panel__summary">
+            <a
+              href="#"
+              class="btn d-flex justify-content-between align-items-center px-4 py-3
+              bg-tertiary rounded-0 position-relative text-white"
+              @click.prevent="openSummary()">
+              <h4 class="mb-0">
+                {{ $t('movements.period-resume') }}
+              </h4>
+              <font-awesome-icon
+                :icon="['fas', activeSummary ? 'times' : 'chevron-up']" />
+            </a>
+            <div
+              v-if="activeSummary === true"
+              class="movements-panel__summary-details border-top bg-white">
+              <h5 class="text-primary px-4 pt-4">
+                <span class="font-weight-light">{{ $t('movements.period') }}:</span>
+                {{ fromDate | date('MMMM yyyy') }}
+                {{ $t('movements.to') }}
+                {{ toDate | date('MMMM yyyy') }}
+              </h5>
+              <div class="d-flex align-items-center border-bottom border-top px-4 py-2">
                 <div>
-                  <h4>
-                    {{ $t('movements.period') }}:
-                    <strong>{{ fromDate | date('MMMM yyyy') }} {{ $t('movements.to') }}</strong>
-                    <strong> {{ toDate | date('MMMM yyyy') }}</strong>
-                  </h4>
+                  {{ $t('movements.cash-balance') }}
                 </div>
-                <div class="align-items-center">
-                  <div>
-                    <strong>{{ $t('movements.cash-balance') }}</strong>
-                  </div>
-                  <div class="ml-auto">
-                    <small class="d-block text-right">{{ $t('movements.initial') }}</small>
-                    <p class="mb-0 text-right">
-                      {{ summary.initialBalance.amount.value | currency }}
-                    </p>
-                  </div>
-                  <div>
-                    <small class="d-block text-right">{{ $t('movements.final') }}</small>
-                    <p class="mb-0 text-right">
-                      {{ summary.endBalance.amount.value | currency }}
-                    </p>
-                  </div>
+                <div class="ml-auto mr-4">
+                  <small class="d-block text-right">{{ $t('movements.initial') }}</small>
+                  <p class="mb-0 text-right">
+                    {{ summary.initialBalance.amount.value | currency }}
+                  </p>
                 </div>
-                <div class="align-items-center">
-                  <div>
-                    <strong>Movimientos</strong>
-                  </div>
-                  <div class="ml-auto">
-                    <small class="d-block text-right">{{ $t('movements.payments') }}</small>
-                    <p class="mb-0 text-right">
-                      {{ summary.deposits.amount.value | currency }}
-                    </p>
-                  </div>
-                  <div>
-                    <small class="d-block text-right">{{ $t('movements.transfers') }}</small>
-                    <p class="mb-0 text-right">
-                      {{ summary.transfers.amount.value | currency }}
-                    </p>
-                  </div>
-                  <div>
-                    <small class="d-block text-right">{{ $t('movements.purchases') }}</small>
-                    <p class="mb-0 text-right">
-                      {{ summary.purchases.amount.value | currency }}
-                    </p>
-                  </div>
-                  <div>
-                    <small class="d-block text-right">{{ $t('movements.sales') }}</small>
-                    <p class="mb-0 text-right">
-                      {{ summary.sales.amount.value | currency }}
-                    </p>
-                  </div>
+                <div>
+                  <small class="d-block text-right">{{ $t('movements.final') }}</small>
+                  <p class="mb-0 text-right">
+                    {{ summary.endBalance.amount.value | currency }}
+                  </p>
+                </div>
+              </div>
+              <div class="d-flex align-items-center px-4 py-2">
+                <div>
+                  Movimientos
+                </div>
+                <div class="ml-auto mr-4">
+                  <small class="d-block text-right">{{ $t('movements.payments') }}</small>
+                  <p class="mb-0 text-right">
+                    {{ summary.deposits.amount.value | currency }}
+                  </p>
+                </div>
+                <div class="mr-4">
+                  <small class="d-block text-right">{{ $t('movements.transfers') }}</small>
+                  <p class="mb-0 text-right">
+                    {{ summary.transfers.amount.value | currency }}
+                  </p>
+                </div>
+                <div class="mr-4 text-right">
+                  <small class="d-block">{{ $t('movements.purchases') }}</small>
+                  <p class="mb-0">
+                    {{ summary.purchases.amount.value | currency }}
+                  </p>
+                </div>
+                <div class="text-right">
+                  <small class="d-block">{{ $t('movements.sales') }}</small>
+                  <p class="mb-0">
+                    {{ summary.sales.amount.value | currency }}
+                  </p>
                 </div>
               </div>
             </div>
@@ -443,7 +481,7 @@ export default {
         });
       } else {
         elems.forEach((elem) => {
-          elem.click();
+          if (elem.checked) { elem.click(); }
         });
       }
     },
@@ -506,5 +544,16 @@ export default {
 @import '../scss/_variables.scss';
 .movements-panel__content {
 background-color: $tertiary-20;
+.movements-panel__row, .movements-panel__summary {
+  .btn.collapsed  {
+    z-index: 100;
+    .svg-inline--fa {
+      transform: rotate(180deg);
+    }
+  }
+  .movements-panel__row-details, .movements-panel__summary-details {
+    background-color: $tertiary-10;
+  }
+}
 }
 </style>
