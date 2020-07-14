d5847510f748bb010bc76cae7296ce907df92a94
Juan Cristobal Pazos <juancri@modyo.com>
2020-07-14 15:47:06 -0400

    refactor: refactor

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  publicPath: '',
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
   pluginOptions: {
     i18n: {
       locale: 'es-CL',
